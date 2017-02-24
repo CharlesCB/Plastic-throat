2a675a7cf87e75abbb40d56809d4c69267475f71
CharlesCB <c.campeaubedford@hotmail.com>
2017-02-24 14:44:30 -0500

    Update Classe synthèse vocale.py

--- Classe synthèse vocale.py
+++ Classe synthèse vocale.py
@@ -20,9 +20,9 @@ i = [250, 2250, 2980, 3280]
 
 
 class Synth:
-    def __init__(self):
-        self.env = Fader(fadein = 0.2, fadeout = 0.5).play()
-        self.harms = random.randint(10,14)
+    def __init__(self, vowel = o, fadein = 0.2, fadeout = 0.5):
+        self.env = Fader(fadein = fadein, fadeout = fadeout).play()
+        self.harms = random.randint(20,25)
         self.vibfreq = Sine(freq = 2, add = 1.5) 
         self.vibr = Sine(freq = self.vibfreq,add = 1, mul = 0.002)
 
@@ -35,9 +35,9 @@ class Synth:
         self.src = Blit(freq = 200 * self.vibr + self.rauque, harms = self.harms, mul = self.env * 2).mix(2)
         self.lp = Biquad(self.src,freq = 350 * self.vibr, q = 1)
         
-        self.bp1 = ButBP(self.lp, freq = o, q = 5, mul = 1.5)
-        self.bp2 = ButBP(self.bp1, freq = o, q = 5, mul = 1.5)
-        self.bp3 = ButBP(self.bp2, freq = o, q = 5, mul = 1.5)
+        self.bp1 = ButBP(self.lp, freq = vowel, q = 5, mul = 1.5)
+        self.bp2 = ButBP(self.bp1, freq = vowel, q = 5, mul = 1.5)
+        self.bp3 = ButBP(self.bp2, freq = vowel, q = 5, mul = 1.5)
         
         self.reverb = Freeverb(self.bp3).out()
 
