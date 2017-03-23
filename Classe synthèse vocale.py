3ea67ad7653a971c4e5f54f4dfec843ee6d93d47
CharlesCB <c.campeaubedford@hotmail.com>
2017-03-23 12:03:00 -0400

    Update Classe synthèse vocale.py

--- Classe synthèse vocale.py
+++ Classe synthèse vocale.py
@@ -1,7 +1,7 @@
 from pyo import *
 import random
 
-s = Server().boot()
+s = Server()
 
 # ténor (125 à 500 Hz // 48 à 72 midi)
 
@@ -21,29 +21,42 @@ i = [250, 2250, 2980, 3280]
 
 class Synth:
     def __init__(self, vowel = o, fadein = 0.2, fadeout = 0.5):
-       # self.note = Notein(poly=10, scale=1, first=0, last=127)
-       # self.pit = self.note['pitch']
+        self.note = Notein(poly=5, scale=1, first=0, last=127)
+        self.pit = self.note['pitch']
+        self.amp = MidiAdsr(self.note['velocity'], attack=0.001,decay=.1, sustain=1, release=1, mul=2)
         
         self.env = Fader(fadein = fadein, fadeout = fadeout).play()
 
-        self.harms = Randi(min=35, max=40, freq=0.25)
+        self.harms = Randi(min=40, max=45, freq=0.25)
 
-        self.vibfreq = Sine(freq = 2, add = 1.5) 
-        self.vibr = Sine(freq = self.vibfreq,add = 1, mul = 0.002 * self.env)
+       ## self.vibfreq = Sine(freq = 2, add = 1.5) 
+        self.vibr = Sine(freq = 2,add = 1, mul = 0.002 * self.env)
 
-        self.tremolo = Sine(freq = 2, add=1, mul = 0.001)
+        self.tremolo = Sine(freq = 2, add=1, mul = 0.01)
 
-        ### As-tu essaye de multiplier la source harmonique (Blit) par le bruit filtre?
-        ### Bien dose, ca aide a donner le sentiment que les deux proviennent de la meme source.
-        self.bruit = PinkNoise(0.05)
+        self.bruit = PinkNoise(mul = 0.005)
         self.rauque = ButBP(self.bruit,freq = 1500)
 
-        self.src = Blit(freq = midiToHz(48) * self.vibr, harms = self.harms, mul = self.env).mix(2)
-        self.src * self.rauque
-        self.lp = Biquadx(self.src,freq = 350 * self.vibr, q = 1, stages=2, mul = 0.5)
+        self.src = Blit(freq = self.pit * self.vibr, harms = self.harms, mul = self.amp * self.tremolo)
         
-        self.formants = Biquadx(self.lp, freq=vowel, q=5, stages=3, mul=0.3).mix(1).out()
+        ## à paufiner
+        self.sum =  self.src * self.rauque
 
-voix = Synth(o)
+        self.lp = Biquadx(self.sum,freq = 350, q = 1, stages=2, mul = 10)
+        
+        self.formants = Biquadx(self.lp, freq=vowel, q=5, stages=3, mul=2).mix(1)
+        
+    def setVowel(self,x):
+        self.formants.freq = x
+        
+    def out(self):
+        self.formants.out()
+        return self
+        
+s.setMidiInputDevice(3) 
+s.boot()
+
+voix = Synth().out()
+
 s.start()
 s.gui(locals())
